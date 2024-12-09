f6911ef91abca530e9af8d4fe262f11ddbb92b27
Charles Murimi <mjohnson@kabarak.ac.ke>
2024-12-09 08:16:14 +0300

    Migration of DB to MySql

--- Tropicana/settings.py
+++ Tropicana/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'Tropicana.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tropicana',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
